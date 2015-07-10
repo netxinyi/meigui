28862a19004338abad89821aa626fc568ada2bcf
netxinyi <vision@netxinyi.com>
2015-07-11 00:10:25 +0800

    程序安装器

---
+++ app/Commands/Installtion.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Bus\SelfHandling;
+use \Cache;
+
+class Installtion extends Command implements SelfHandling
+{
+
+
+    protected $name        = "install";
+
+    protected $description = "一键安装程序并配置";
+
+
+    public function __construct()
+    {
+
+        parent::__construct();
+    }
+
+
+    public function handle()
+    {
+
+        $this->info('开始安装程序，请稍后...');
+        //安装数据库
+        //  $this->call('migrate');
+        $this->info('数据库安装完成');
+        //设置缓存
+        Cache::forever('visits', 1);
+        $this->info('基础配置完成');
+    }
+}
