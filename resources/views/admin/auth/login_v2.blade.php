634fa278ab10051b6360134166fab3439812d6bf
netxinyi <vision@netxinyi.com>
2015-07-11 10:38:30 +0800

    登录页调整

--- resources/views/admin/auth/login_v2.blade.php
+++ resources/views/admin/auth/login_v2.blade.php
@@ -1,7 +1,28 @@
-@extends('admin.master')
+@extends('layouts.layout')
 
 @section('title')
     登录-后台管理中心-{{config('app.site_name')}}
+@stop
+@section('global-css')
+
+    <!-- ================== BEGIN BASE CSS STYLE ================== -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <link href="/assets/lib/jquery-ui/themes/base/minified/jquery-ui.min.css" rel="stylesheet"/>
+    <link href="/assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="/assets/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet"/>
+    <link href="/assets/admin/css/animate.min.css" rel="stylesheet"/>
+    <link href="/assets/admin/css/style.min.css" rel="stylesheet"/>
+    <link href="/assets/admin/css/style-responsive.min.css" rel="stylesheet"/>
+    <link href="/assets/admin/css/theme/default.css" rel="stylesheet" id="theme"/>
+    <!-- ================== END BASE CSS STYLE ================== -->
+
+@stop
+
+@section('header-global-js')
+    <!-- ================== BEGIN BASE JS ================== -->
+    <script src="/assets/lib/pace/pace.min.js"></script>
+    <!-- ================== END BASE JS ================== -->
+
 @stop
 
 @section('body')
@@ -73,6 +94,28 @@
 
 @overwrite
 
+@section('footer-global-js')
+    <!-- ================== BEGIN BASE JS ================== -->
+    <script src="/assets/lib/jquery/jquery-1.9.1.min.js"></script>
+    <script src="/assets/lib/jquery/jquery-migrate-1.1.0.min.js"></script>
+    <script src="/assets/lib/jquery-ui/ui/minified/jquery-ui.min.js"></script>
+    <script src="/assets/lib/bootstrap/js/bootstrap.min.js"></script>
+    <!--[if lt IE 9]>
+    <script src="/assets/admin/crossbrowserjs/html5shiv.js"></script>
+    <script src="/assets/admin/crossbrowserjs/respond.min.js"></script>
+    <script src="/assets/admin/crossbrowserjs/excanvas.min.js"></script>
+    <![endif]-->
+    <script src="/assets/lib/slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="/assets/lib/jquery-cookie/jquery.cookie.js"></script>
+    <script src="/assets/admin/js/apps.min.js"></script>
+    <!-- ================== END BASE JS ================== -->
+    <script>
+        $(document).ready(function () {
+            App.init();
+        });
+    </script>
+@stop
+
 @section('footer-last-js')
     <!-- ================== BEGIN PAGE LEVEL JS ================== -->
     <script src="/assets/admin/js/login-v2.demo.min.js"></script>
@@ -82,4 +125,6 @@
             LoginV2.init();
         });
     </script>
-@overwrite
+@stop
+
+@section('body-class')pace-top @stop
