e7764fc0d4939e74fae4c28e9b35d0a5568bcd59
netxinyi <vision@netxinyi.com>
2015-09-04 23:27:51 +0800

    微信大屏幕界面

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -10,23 +10,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Model\Option;
-use Overtrue\LaravelWechat;
+use Overtrue\Wechat\Server as Server;
+use Overtrue\Wechat\Message as Message;
 
 class WechatController extends Controller
 {
 
 
-    protected $wx;
+    protected $wechat;
 
 
     public function __construct(Request $request)
     {
 
+        header('Access-Control-Allow-Origin: *');
 
         $options = Option::whereIn('key', ['wx_app_id', 'wx_token', 'wx_encoding_key', 'wx_app_secret'])->lists('value',
             'key');
-
-
+        $this->wechat = new Server($options['wx_app_id'], $options['wx_token']);
 
     }
 
@@ -34,6 +35,13 @@ class WechatController extends Controller
     public function index()
     {
 
+        $this->wechat->on('message', 'text', function (){
+
+            return Message::make()->content('您好');
+        });
+
+
+        return $this->wechat->serve();
     }
 
 
---
+++ config/wechat.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'use_alias'    => false,
+    'app_id'       => 'wx9faebf373036d2f2', // 必填
+    'secret'       => 'de023483d8b5b6381dff8efab164578c', // 必填
+    'token'        => 'd971905d7aab85f',  // 必填
+    'encoding_key' => '' // 加密模式需要，其它模式不需要
+    //'encoding_key' => 'eyGWIDMhOCETXiMnoJoH0iZkLkBNq9FIi6zcTAtmemR' // 加密模式需要，其它模式不需要
+];
