da5ee0fb5ce60d6f9d96965793b8bb8b15cb8a3e
netxinyi <vision@netxinyi.com>
2015-12-05 18:20:06 +0800

    object支持lang

--- app/Model/UserObject.php
+++ app/Model/UserObject.php
@@ -8,6 +8,8 @@
 namespace App\Model;
 
 
+use App\Enum\User as UserEnum;
+
 class UserObject extends BaseModel
 {
 
@@ -26,4 +28,94 @@ class UserObject extends BaseModel
     protected $primaryKey = 'user_id';
 
     public $timestamps = false;
+
+    /**
+     * 追加属性
+     * @var array
+     */
+    protected $append = ['sex_lang', 'age_lang', 'house_lang', 'height_lang', 'level_lang', 'education_lang', 'salary_lang', 'marriage_lang'];
+
+    /*
+    |--------------------------------------------------------------------------
+    | 访问器
+    |--------------------------------------------------------------------------
+    */
+
+    protected function getLang($langArray, $attr, $default = '不限')
+    {
+        if (array_has($this->attributes[$attr])) {
+            return $langArray[$this->attributes[$attr]];
+        }
+        return $default;
+    }
+
+    public function getHouseLangAttribute()
+    {
+        return $this->getLang(UserEnum::$houseLang, 'house');
+    }
+
+    public function getSexLangAttribute()
+    {
+        return UserEnum::$sexLang[$this->attributes['sex']];
+    }
+
+    public function getLevelLangAttribute()
+    {
+        return $this->getLang(UserEnum::$levelLang, 'level');
+    }
+
+    public function getEducationLangAttribute()
+    {
+        return $this->getLang(UserEnum::$educationLang, 'education');
+    }
+
+    public function getMarriageLangAttribute()
+    {
+        return $this->getLang(UserEnum::$marriageLang, 'marriage');
+    }
+
+    public function getSalaryLangAttribute()
+    {
+        $start = array_get($this->attributes, 'salary_start');
+        $end = array_get($this->attributes, 'salary_end');
+        if ($start && $end) {
+            return $start . ' 到 ' . $end;
+        } elseif ($start) {
+            return $start . '以上';
+        } elseif ($end) {
+            return $end . '以下';
+        }
+
+        return '不限';
+    }
+
+    public function getHeightLangAttribute()
+    {
+        $start = array_get($this->attributes, 'height_start');
+        $end = array_get($this->attributes, 'height_end');
+        if ($start && $end) {
+            return $start . ' 到 ' . $end . ' cm';
+        } elseif ($start) {
+            return $start . 'cm 以上';
+        } elseif ($end) {
+            return $end . 'cm 以下';
+        }
+
+        return '不限';
+    }
+
+    public function getAgeLangAttribute()
+    {
+        $start = array_get($this->attributes, 'age_start');
+        $end = array_get($this->attributes, 'age_end');
+        if ($start && $end) {
+            return $start . ' 到 ' . $end . ' 岁';
+        } elseif ($start) {
+            return $start . '岁以上';
+        } elseif ($end) {
+            return $end . '岁以下';
+        }
+
+        return '不限';
+    }
 }
