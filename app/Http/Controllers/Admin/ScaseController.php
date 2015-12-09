8933c200f653a102fae8274bc24637622f220c59
netxinyi <vision@netxinyi.com>
2015-12-09 22:40:10 +0800

    案例文件上传

--- app/Http/Controllers/Admin/ScaseController.php
+++ app/Http/Controllers/Admin/ScaseController.php
@@ -69,7 +69,7 @@ class ScaseController extends Controller
     }
 
 	public function edit(Scase $scase){
-		$scase['photos'] = explode('\n',$scase['photos']);
+        $scase['photos'] = explode("\n", $scase['photos']);
 		return $this->view('edit')->with('case',$scase);
 	}
 
