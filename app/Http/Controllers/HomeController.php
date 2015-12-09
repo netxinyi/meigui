1402543edf064d1c7aa8dc2ac2ebae1de4128d8c
netxinyi <vision@netxinyi.com>
2015-12-09 23:25:38 +0800

    上传头像

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -124,6 +124,139 @@ class HomeController extends Controller
 
     public function postPhoto()
     {
+        // Make sure file is not cached (as it happens for example on iOS devices)
+                header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+                header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+                header("Cache-Control: no-store, no-cache, must-revalidate");
+                header("Cache-Control: post-check=0, pre-check=0", false);
+                header("Pragma: no-cache");
+
+                /*
+                  // Support CORS
+                  header("Access-Control-Allow-Origin: *");
+                  // other CORS headers if any...
+                  if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+                  exit; // finish preflight CORS requests here
+                  }
+                 */
+
+                // 5 minutes execution time
+                @set_time_limit(5 * 60);
+
+                //$targetDir = '/uploads/user_pic'; //5.1调用
+
+                 
+               $targetDir = 'uploads/avatar';
+                $cleanupTargetDir = true; // Remove old files
+                $maxFileAge = 5 * 3600; // Temp file age in seconds    
+                // echo "<script>alert(111);</script>";
+                // Create target dir
+                //判断是不是有这么一个目录，如果没有，那就递归创建这个目录
+                // dd($targetDir);
+                if (!is_dir($targetDir)) {
+                    mkdir($targetDir, 0777, true);chmod($targetDir,0777);
+                }
+
+                // Get a file name
+                if (isset($_REQUEST["name"])) {
+
+                    $fileName = $_REQUEST["name"];
+                } elseif (!empty($_FILES)) {
+                    $fileName = $_FILES["file"]["name"];
+                } else {
+                    $fileName = uniqid("file_");
+                }
+                // dd($fileName);
+
+               
+                $filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
+
+                // Chunking might be enabled
+                $chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
+                $chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
+
+
+                // Remove old temp files  
+                if ($cleanupTargetDir) {
+
+                    if (!is_dir($targetDir) || !$dir = opendir($targetDir)) {
+                        die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory(未能打开临时目录)."}, "id" : "id"}');
+                    }
+
+                    while (($file = readdir($dir)) !== false) {
+                        $tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
+
+                        // If temp file is current file proceed to the next
+                        if ($tmpfilePath == "{$filePath}.part") {
+                            continue;
+                        }
+
+                        // Remove temp file if it is older than the max age and is not the current file
+                        if (preg_match('/\.part$/', $file) && (filemtime($tmpfilePath) < time() - $maxFileAge)) {
+                            @unlink($tmpfilePath);
+                        }
+                    }
+                    closedir($dir);
+                }
+
+
+                // Open temp file
+                if (!$out = @fopen("{$filePath}.part", $chunks ? "ab" : "wb")) {
+                    die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+                }
+
+                if (!empty($_FILES)) {
+                    if ($_FILES["file"]["error"] || !is_uploaded_file($_FILES["file"]["tmp_name"])) {
+                        die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+                    }
+
+                    // Read binary input stream and append it to temp file
+                    if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
+                        die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+                    }
+                } else {
+                    if (!$in = @fopen("php://input", "rb")) {
+                        die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+                    }
+                }
+
+                while ($buff = fread($in, 4096)) {
+                    fwrite($out, $buff);
+                }
+
+
+                @fclose($out);
+                @fclose($in);
+
+                // Check if file has been uploaded
+                if (!$chunks || $chunk == $chunks - 1) {
+                    // Strip the temp .part suffix off 
+                    rename("{$filePath}.part", $filePath);
+                }
+                // Return Success JSON-RPC response
+                //处理要存入数据库的路径
+                // $filePath = strchr($filePath,"answer");
+                if (strpos($filePath, 'uploads')) {
+
+                    $filePath = strchr($filePath, "/uploads");
+
+                }
+                $filePath = substr($filePath,15);
+              
+                return $filePath;
+                //            die('{"jsonrpc" : "2.0", "result" : null, "id" : "id"}');
 
     }
+
+
+    public function postTouxiang(){
+         //接收数据
+       
+        $data = $this->request()->only('avatar');
+        user()->update($data);
+
+        return $this->rest()->success('保存成功');
+
+    }
+
 }
