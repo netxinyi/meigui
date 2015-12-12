e46ca94cf1cf63f8b31a4db351714512f9fbd009
netxinyi <vision@netxinyi.com>
2015-12-12 21:10:27 +0800

    会员默认头像和照片

--- app/Model/User.php
+++ app/Model/User.php
@@ -92,7 +92,7 @@ class User extends BaseModel implements AuthenticatableContract, CanResetPasswor
     public function getAvatarAttribute()
     {
 
-        if (!$this->attributes['avatar']) {
+        if (!$this->attributes['avatar'] || !\File::exists(public_path() . '/uploads/avatar/' . $this->attributes['avatar'])) {
             if ($this->attributes['sex'] == UserEnum::SEX_FEMALE) {
                 return asset('/assets/images/default-avatar-female.jpg');
             }
