dcc67845b08fe620154dd06420b54c0d7bd3c3c5
netxinyi <vision@netxinyi.com>
2015-12-14 13:38:40 +0800

    修复微信

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -157,9 +157,10 @@ class WechatController extends Controller
      *
      * @return string
      */
-    public function anyApi(Server $server)
+    public function anyApi()
     {
         $url = Config::get('app.url');
+
         $server = new Server(option('wechat_app_id'), option('wechat_token'), option('wechat_encode_key'));
         //关注回复
         $server->on('event', 'subscribe', function ($event) {
